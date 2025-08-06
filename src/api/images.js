48591971017271c1cb5ab9079b675c7a68a5c9bc
Berke <zberke97@gmail.com>
2025-08-06 13:58:19 +0300

    Move API key to .env and remove it from repo

--- src/api/images.js
+++ src/api/images.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://api.unsplash.com/search/photos";
-const API_KEY = "Z5xm-39WzWSlxTvC1yg9uRaw9OTVoaO8Md9chnl-lGQ";
+const API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
 
 export const fetchImages = async (query, page = 1) => {
   const response = await axios.get(API_URL, {
